2eef0a8685e3c46f554cb925a69d41332377b955
Seth Snel <s.snel@s-bb.nl>
2022-12-05 10:49:52 +0100

    Updated favicon size

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -30,7 +30,7 @@ function ZaalvoetbalbazenApp({ Component, pageProps }: AppProps) {
     <Head>
       <title>Zaalvoetbalbazen</title>
       <meta name="description" content="Zaalvoetbalbazen" />
-      <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%22100%22>⚽</text></svg>"></link>
+      <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2280%22>⚽</text></svg>"></link>
     </Head>
     {
       (isLoading) ?
