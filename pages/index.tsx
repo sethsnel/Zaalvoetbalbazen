2f1284063b5531449aee1a2e33a6a50c0268b1a0
Seth Snel <s.snel@s-bb.nl>
2022-12-07 17:38:11 +0100

    Updated present indicator

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,10 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import dayjs from 'dayjs'
 import Link from 'next/link'
-import { MdOutlineEventAvailable, MdOutlineEventBusy } from 'react-icons/md'
-import { RiQuestionMark } from 'react-icons/ri'
+import Image from 'next/image'
+import { BsCheckCircleFill, BsQuestionCircleFill, BsXCircleFill } from 'react-icons/bs'
 
-import { useSeasonDates, useSessions } from '../lib/seasonDBO'
+import { useMyProfile, useSeasonDates, useSessions } from '../lib/seasonDBO'
 import { useAppSettings } from '../lib/appSettingsDBO'
 import { useUser } from '../lib/useUser'
 
@@ -18,11 +18,27 @@ const Home: NextPage = () => {
   const { sessions } = useSessions(appSettings?.activeSeason || '')
   const [upcommingDate, ...commingWeeks] = Object.values(seasonDates || {}).filter(date => date > dayjs().add(-12, 'hours').unix() && date < dayjs().add(4, 'weeks').unix())
   const later = Object.values(seasonDates || {}).filter(date => date > dayjs().add(4, 'weeks').unix())
+  const { profile, isLoading: isLoadingProfile } = useMyProfile(appSettings?.activeSeason || '', user?.id || '')
 
   const getMyBadge = (session: { [key: string]: { isPresent: boolean } }) => {
-    if (!session || !session[user?.id || '']) return <span className="text-primary fs-5 d-flex"><RiQuestionMark /></span>
-    if (session[user?.id || '']?.isPresent) return <span className="text-success fs-5 d-flex"><MdOutlineEventAvailable /></span>
-    return <span className="text-danger fs-5 d-flex"><MdOutlineEventBusy /></span>
+    if (!session || !session[user?.id || '']) {
+      return <div className="position-absolute end-0 top-50 translate-middle me-5 opacity-50">
+      <Image src={profile?.profilePic || 'https://craftsnippets.com/articles_images/placeholder/placeholder.jpg'} height={30} width={30} className={styles.picture} objectFit='cover' />
+      {/* <span className="text-primary fs-5 position-absolute top-0 start-100 translate-middle"><BsQuestionCircleFill /></span> */}
+    </div>
+    }
+    
+    if (session && session[user?.id || '']?.isPresent) {
+      return <div className="position-absolute end-0 top-50 translate-middle me-5">
+      <Image src={profile?.profilePic || 'https://craftsnippets.com/articles_images/placeholder/placeholder.jpg'} height={30} width={30} className={styles.picture} objectFit='cover' />
+      <span className="text-success fs-5 position-absolute top-0 start-100 translate-middle"><BsCheckCircleFill /></span>
+    </div>
+    }
+
+    return <div className="position-absolute end-0 top-50 translate-middle me-5">
+      <Image src={profile?.profilePic || 'https://craftsnippets.com/articles_images/placeholder/placeholder.jpg'} height={30} width={30} className={styles.picture} objectFit='cover' />
+      <span className="text-danger fs-5 position-absolute top-0 start-100 translate-middle"><BsXCircleFill /></span>
+    </div>
   }
 
   return (
@@ -60,6 +76,7 @@ const Home: NextPage = () => {
                   date={date}
                   sessions={sessions}
                   limit={appSettings?.sessionLimit}
+                  presentIndicator={true}
                   badge={getMyBadge(sessions[date])} />
               </div>)
           }
